a213cc91696679bf982b152bc1e7d4b883909084
Tevin Hamilton <tahamilton@student.fullsail.edu>
2020-10-17 06:45:53 -0500

    Adding SDI Code exercise 01

---
+++ HamiltonTevin_Lab1/HamiltonTevin_Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamiltonTevin_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Tevin Hamilton");
+            Console.WriteLine("my degree program is moblie devevolpment");
+            Console.WriteLine("I love watching and looking up everying that relates to star wars");
+            Console.WriteLine("I love shooting guns on the range");
+            Console.WriteLine("If stranded on a desert island then coconut water would be my main soures to drink");
+            Console.WriteLine("If stranded on a desert island then i would read star wars revenge of the sith");
+            Console.WriteLine(value: "If stranded on a desert island then i would listen to nf perception");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SDI_CE01
  This assignment is my first ever coding assignment at full sail writing to the console 
+## Console writelines 
+In this assingmeent i was to answer questions given to me by the teacher which included my name , degree program, what i like to do and so forth.
